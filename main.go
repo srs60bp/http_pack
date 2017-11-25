94c6d292df4586dd170ee75b966ef52115f12d9b
srs60 <srs60@abv.bg>
2017-11-26 00:34:06 +0200

    _

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error: ",err)
+		os.Exit(1)
+	}
+}

d7adbaaea7613bf348448300e4ccb2d36fb6716e
srs60 <srs60@abv.bg>
2017-11-26 02:21:38 +0200

    update 2

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
-		fmt.Println("Error: ",err)
+		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
+	//fmt.Println(resp)
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(bs)
+
 }
